ffdcb05d22f7ae20b8e181847d206f6ea9a80e50
heyder <heyder@alligatorteam.org>
2013-01-31 18:59:10 -0300

    code refactor

--- library/manparse.rb
+++ library/manparse.rb
@@ -1,14 +1,18 @@
 class ManParser
 
- #attr_accessor :net, :port
-  
-  def initialize()
+  attr_accessor :bin, :man, :opts
+
+  def initialize(bin)
+    (bin.match(%r{\/(\w+)$}) ? @bin = $1 : @bin = bin)
+    @man = nil
+    @buff = nil
+    @opts = []
+    rescue Exception
   end
 
-  def searchMan(binary)
+
+  def getMan()
     manPath = "/usr/share/man/"
-    
-    (binary.match(%r{\/(\w+)$}) ? binary = $1 : binary)
 
 #    1      User Commands
 #    2      System Calls
@@ -21,27 +25,37 @@ class ManParser
 
     sections = [1,8,2,3,4,5,6,7]
     sections.each do |s|
-      man = "#{manPath}man#{s}/#{binary}.#{s}.gz"
+      man = "#{manPath}man#{s}/#{@bin}.#{s}.gz"
       if (File.file?(man))
-        return man
+        @man = man
+        return true
+      else
+        return false
       end
     end
   rescue Exception
   end
-
-  def getBinName(man)
-    man.match(%r{^.B\s(.+)$}i).captures
-    p = $1 unless $1.nil?
-    return p
-  end 
-
-  def getOpts(man)
-    opts = []
-    man.scan(%r{(\-?\\-[\w\d]+)}).each do |l|
-      opts << l.to_s.gsub!('\\','')
-     end
-    return opts.uniq.inspect
+  
+  def inflate()
+    zstream  = Zlib::GzipReader.new(File.open(@man))
+    buf = zstream.read
+    zstream.close
+    return buf
   end
 
+=begin
+ def getBinName(man)
+    man.match(%r{^.B\s(.+)$}i).captures
+    p = $1 unless $1.nil?
+    return p
+  end
+=end
+
+  def getOpts()
+    @buff = inflate()
+    @buff.scan(%r{(\-?\\-[\w\d]+)}).each do |l|
+      @opts << l.to_s.gsub!('\\','')
+     end
+  end
 
 end
